118473f2fd61efc556002e6890b8ad9fb3e1c629
Dominik Winecki <dominikwinecki@gmail.com>
2019-09-06 22:30:01 -0400

    Basic repl

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ mod ops;
 mod tokenizer;
 mod value;
 
+use std::io::{self, Write};
+use expression::Expression;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut line = String::new();
+        print!("repl> ");
+        io::stdout().flush();
+        io::stdin().read_line(&mut line).unwrap();
+        let expr: Expression = Expression::from(line.as_str());
+        println!("{}", expr.eval());
+    }
 }
--- src/value.rs
+++ src/value.rs
@@ -1,4 +1,14 @@
+use std::fmt;
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Value {
     Integer(i32),
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Value::Integer(int) => write!(f, "{}", int)
+        }
+    }
 }
