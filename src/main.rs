654386740ae46ae5f6fd5a944c004a83a1715063
Dominik Winecki <dominikwinecki@gmail.com>
2019-09-08 12:50:21 -0400

    Add multiline repl support

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,25 @@ use std::io::{self, Write};
 
 fn main() {
     let mut context = Context::new();
+    let mut line = String::new();
     loop {
-        let mut line = String::new();
-        print!("repl> ");
+        if line.is_empty() {
+            print!("repl> ");
+        }
         io::stdout().flush().unwrap();
         if io::stdin().read_line(&mut line).unwrap() == 0 {
             println!();
             break;
         }
         if line.is_empty() || line == "\n" {
+            line.clear();
+            continue;
+        }
+        if line.chars().filter(|c| *c == '(').count() > line.chars().filter(|c| *c == ')').count() {
             continue;
         }
         let expr: Expression = Expression::from(line.as_str());
+        line.clear();
         println!("{}", expr.eval(&mut context));
     }
 }
